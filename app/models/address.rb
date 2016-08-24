bc6fc4cef1e49d3c1ce2444db71517dcefef8dec
nori <nori@nori-MacBook-Pro.local>
2016-08-24 17:05:20 +0900

    model validation

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,10 @@
 class Address < ActiveRecord::Base
   belongs_to :addressable, polymorphic: true
+  validate :dealing_address
+
+  def dealing_address
+    if addressable_type == "Dealing"
+      validates :first_name, :family_name, :street_address, :post_number, :order_email, presence: true
+    end
+  end
 end
--- app/models/dealing.rb
+++ app/models/dealing.rb
@@ -2,4 +2,5 @@ class Dealing < ActiveRecord::Base
   belongs_to :book
   has_one :address, as: :addressable
   accepts_nested_attributes_for :address
+  validates :registered, acceptable: true
 end
