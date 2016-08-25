f15c06b5f9671e6278e6fa720a2e93fd037ac796
nori <nori@nori-MacBook-Pro.local>
2016-08-25 15:01:01 +0900

    DB reset

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+json = ActiveSupport::JSON.decode(File.read('books.json'))
+
+# 変数jsonに入った配列状態のjsonデータを一つ一つ取り出して、モデル.createを使ってdbに投入
+json.each do |data|
+  Book.create(data)
+end
