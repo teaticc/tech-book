b57ac4130afd748d08c6cc9d84e84f2712d43c43
nori <nori@nori-MacBook-Pro.local>
2016-08-25 13:15:43 +0900

    Log in flash

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,13 +2,22 @@ class Users::SessionsController < Devise::SessionsController
 before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  def new
+    self.resource = resource_class.new(sign_in_params)
+    clean_up_passwords(resource)
+    yield resource if block_given?
+    # respond_with(resource, serialize_options(resource))
+    render template: "users/sessions/new"
+  end
 
   # POST /resource/sign_in
   def create
-    super
+    self.resource = warden.authenticate!(auth_options)
+    set_flash_message!(:notice, :signed_in)
+    sign_in(resource_name, resource)
+    yield resource if block_given?
+    # respond_with resource, location: after_sign_in_path_for(resource)
+    redirect_to :back
   end
 
   # DELETE /resource/sign_out
