03591abba2033211bf26ea1328d21e4926fae353
nori <nori@nori-MacBook-Pro.local>
2016-08-25 22:08:34 +0900

    mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:               'smtp.gmail.com',
     port:                  587,
-    domain:                'gmail.com',
+    domain:                'tech-book-heroku.herokuapp.com',
     user_name:             ENV["GOOGLE_ACCOUNT"],
     password:              ENV["GOOGLE_ACCOUNT_PASS"],
     authentication:        'plain',
