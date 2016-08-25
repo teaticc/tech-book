116b5b80ccfac849c6e0841b5869dc3b96ba0433
nori <nori@nori-MacBook-Pro.local>
2016-08-25 17:37:10 +0900

    validation for multiple dealing and save address if user doesn't register it

--- app/controllers/dealings_controller.rb
+++ app/controllers/dealings_controller.rb
@@ -3,7 +3,7 @@ class DealingsController < ApplicationController
     @book = Book.find(params[:book_id])
     @dealing = Dealing.new
     if current_user.address.present?
-      @dealing.build_address(current_user.address.attributes)
+      @dealing.build_address(current_user.address.attributes.except("id", "created_at", "updated_at"))
     else
       @dealing.build_address
     end
@@ -15,6 +15,7 @@ class DealingsController < ApplicationController
       @address = @dealing.address
       @book = @dealing.book
       @book.update_attributes(buyer_id: current_user.id)
+      user_address_check
       DealingMailer.dealing_email(@book, @address).deliver_now
     else
       redirect_to :back, flash: { error: @dealing.errors.full_messages }
@@ -24,5 +25,12 @@ class DealingsController < ApplicationController
   private
   def dealing_params
     params.require(:dealing).permit(:registered, address_attributes: [:family_name, :first_name, :order_email, :phone_number, :post_number, :street_address]).merge(book_id: params[:book_id])
+  end
+
+  def user_address_check
+    if current_user.address.blank?
+      binding.pry
+      current_user.create_address(@address.attributes.except("id", "created_at", "updated_at"))
+    end
   end
 end
