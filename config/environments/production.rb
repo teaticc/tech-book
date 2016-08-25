99eb2df03d39ff44db6b6575638743f0b41a0f9f
nori <nori@nori-MacBook-Pro.local>
2016-08-25 22:30:55 +0900

    Send grid instead of gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,12 +83,12 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default charset: "utf-8"
   config.action_mailer.smtp_settings = {
-    address:               'smtp.gmail.com',
-    port:                  587,
-    domain:                'tech-book-heroku.herokuapp.com',
-    user_name:             ENV["GOOGLE_ACCOUNT"],
-    password:              ENV["GOOGLE_ACCOUNT_PASS"],
-    authentication:        'plain',
-    enable_starttls_auto:  true
-  }
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
 end
