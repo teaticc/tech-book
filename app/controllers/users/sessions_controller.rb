28f13f1a2ff7df24beba71d510803310a12b9079
nori <nori@nori-MacBook-Pro.local>
2016-08-26 13:07:59 +0900

    delete pry

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -12,7 +12,6 @@ before_action :configure_sign_in_params, only: [:create]
 
   # POST /resource/sign_in
   def create
-    binding.pry
     self.resource = warden.authenticate!(auth_options)
     set_flash_message!(:notice, :signed_in)
     sign_in(resource_name, resource)
