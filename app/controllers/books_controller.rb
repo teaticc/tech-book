714feee58c0ff4b4df743f5ee03c4a7812390f5a
nori <nori@nori-MacBook-Pro.local>
2016-08-28 21:05:56 +0900

    put searching description to private

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,20 +6,32 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
-    @related_books = Book.tagged_with(@book.category_list, any: true).uniq.page(params[:page])
+    @related_books = Book.tagged_with(@book.category_list, any: true)
+                         .uniq
+                         .page(params[:page])
   end
 
   def search
-    if params[:category].empty?
-      @books = Book.where("title LIKE ?", "%#{params[:keyword]}%")
-    else
-      @books = Book.tagged_with(params[:category])
-      if params[:keyword].present?
-        @books = @books.where("title LIKE ?", "%#{params[:keyword]}%")
-      end
-    end
+    books_search
     @searcher = { category: params[:category], keyword: params[:keyword] }
     @categories = ActsAsTaggableOn::Tag.all.order(taggings_count: :desc)
-    @books = @books.page(params[:page])
+  end
+
+  private
+
+  def books_search
+    if params[:category].present?
+      if params[:keyword].present?
+        @books = Book.tagged_with(params[:category])
+                     .where("title LIKE ?", "%#{params[:keyword]}%")
+                     .page(params[:page])
+      else
+        @books = Book.tagged_with(params[:category])
+                     .page(params[:page])
+      end
+    else
+      @books = Book.where("title LIKE ?", "%#{params[:keyword]}%")
+                   .page(params[:page])
+    end
   end
 end
