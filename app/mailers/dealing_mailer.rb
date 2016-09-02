8a898117f0f43add28298c3e2e9a370173d0ed7b
nori <nori@nori-MacBook-Pro.local>
2016-09-02 18:27:01 +0900

    mail handling

--- app/mailers/dealing_mailer.rb
+++ app/mailers/dealing_mailer.rb
@@ -1,7 +1,13 @@
 class DealingMailer < ApplicationMailer
   default from: "mailertestforrails@gmail.com"
 
-  def dealing_email(book, address)
+  def bank_email(book, address)
+    @book = book
+    @address = address
+    mail to: @address.order_email, subject: "ご注文を承りました"
+  end
+
+  def credit_email(book, address)
     @book = book
     @address = address
     mail to: @address.order_email, subject: "ご注文を承りました"
