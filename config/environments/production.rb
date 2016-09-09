16770faabec82e4fab3f1eca6808195a4dda665b
nori <nori@nori-MacBook-Pro.local>
2016-09-09 13:30:14 +0900

    config to send email by gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,18 +77,18 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: "tech-book-heroku.herokuapp.com"}
+  config.action_mailer.default_url_options = { host: "52.196.72.59"}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default charset: "utf-8"
   config.action_mailer.smtp_settings = {
-  :address        => 'smtp.sendgrid.net',
+  :address        => 'smtp.gmail.com',
   :port           => '587',
   :authentication => :plain,
-  :user_name      => ENV['SENDGRID_USERNAME'],
-  :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => 'heroku.com',
+  :user_name      => ENV['GOOGLE_ACCOUNT'],
+  :password       => ENV['GOOGLE_ACCOUNT_PASS'],
+  :domain         => 'smtp.gmail.com',
   :enable_starttls_auto => true
 }
 end
